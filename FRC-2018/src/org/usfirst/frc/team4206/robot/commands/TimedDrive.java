8ae4c8fce46a0b8d406dd63d5b4820c92570deb8
student <student@DESKTOP-0B26MKB.nolan.local>
2018-01-16 16:23:06 -0600

    Add dumb autonomous control

--- FRC-2018/src/org/usfirst/frc/team4206/robot/commands/TimedDrive.java
+++ FRC-2018/src/org/usfirst/frc/team4206/robot/commands/TimedDrive.java
@@ -1,40 +1,50 @@
 package org.usfirst.frc.team4206.robot.commands;
 
-import org.usfirst.frc.team4206.robot.OI;
 import org.usfirst.frc.team4206.robot.Robot;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * This command drives the robot with the driver joystick
+ * This command autonomously drives the robot forward at a fixed level of power for a given amount of time
  */
-public class PlayerDrive extends Command {
+public class TimedDrive extends Command {
 
-    public PlayerDrive() {
+	private long time;
+	private double duration, pwr;
+	
+    public TimedDrive(double length, double power) {
         requires(Robot.drivetrain);
+        duration = length;
+        pwr = power;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	time = System.currentTimeMillis();
+    	time += duration;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.arcadeDrive(OI.driver.getRawAxis(OI.leftY), OI.driver.getRawAxis(OI.rightX));
+    	Robot.drivetrain.arcadeDrive(pwr, 0);
     	Timer.delay(0.005);
-	}
+    }
+
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+        if (System.currentTimeMillis() >= time) return true;
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.drivetrain.arcadeDrive(0, 0);
     }
-    
+
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.drivetrain.arcadeDrive(0, 0);
     }
 }
